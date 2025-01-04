7fdd578c7b63d837c51fcfdc6d3d4861c64a277c
Puja-1998 <ghodkepooja98@gmail.com>
2025-01-04 23:48:18 +0530

    Added

--- pressKeyboardKey/script.js
+++ pressKeyboardKey/script.js
@@ -1,12 +1,24 @@
-const charID = document.querySelector("#charID");
+//const charID = document.querySelector("#charID");
 const keyNote = document.querySelector("#keyNote");
-
+const container = document.querySelector(".container");
 const a = "You Pressed "
 
 document.addEventListener("keypress", (event)=>{
+    const key = event.key; 
     
-    keyNote.innerHTML =   a + event.key;
-    charID.innerHTML=event.charCode;
-    
+    keyNote.innerHTML =   a + key;
+   //a.style.color = 'green';
+    //charID.innerHTML=event.charCode;
 
+    // Create a new <h1> element
+    const charID = document.createElement('h2');
+
+    // Set the content for the <h1> element
+    charID.innerHTML = event.charCode;
+    
+    // Optionally, you can add a class or style
+    charID.style.color = 'green';
+
+    container.appendChild(charID);
+    
 })
